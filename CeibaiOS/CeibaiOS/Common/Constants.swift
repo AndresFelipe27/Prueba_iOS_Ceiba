fd7f29d2b67b7a79b1d6c7768ef99803de6d63c4
AndresFelipe27 <anndresfee@hotmail.com>
2020-10-28 16:36:33 -0500

    Added endPoints and constants

---
+++ CeibaiOS/CeibaiOS/Common/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  CeibaiOS
+//
+//  Created by Felipe18 on 28/10/20.
+//  Copyright © 2020 Felipe18. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let protocolo: String = "https://"
+    static let domain: String = "jsonplaceholder.typicode.com"
+    static let baseUrl: String = Constants.protocolo + Constants.domain
+}
---
+++ CeibaiOS/CeibaiOS/Service/EndPoints.swift
@@ -0,0 +1,18 @@
+//
+//  EndPoints.swift
+//  CeibaiOS
+//
+//  Created by Felipe18 on 28/10/20.
+//  Copyright © 2020 Felipe18. All rights reserved.
+//
+
+import Foundation
+
+enum userEndPoints: String {
+    case getUsers = "/users"
+}
+
+enum postEndPoints: String {
+    case getPosts = "/posts"
+    case getPostsByUserId = "/posts?userId="
+}
