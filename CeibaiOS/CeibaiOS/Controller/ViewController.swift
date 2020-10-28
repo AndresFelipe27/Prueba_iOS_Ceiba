0f6bf1720c311ea4809a5c284b790f8cc3702160
AndresFelipe27 <anndresfee@hotmail.com>
2020-10-28 14:36:29 -0500

    Added project structure

---
+++ CeibaiOS/CeibaiOS/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CeibaiOS
+//
+//  Created by Felipe18 on 28/10/20.
+//  Copyright © 2020 Felipe18. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
